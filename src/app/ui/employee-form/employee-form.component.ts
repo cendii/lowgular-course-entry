0f5acfe1456d2867157313c21f23f65c682f2f0b
katarzynacendlak <kasia.cendlak@gmail.com>
2022-11-18 08:37:20 +0100

    feat: form validation

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -10,6 +10,6 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
     name: new FormControl (null, [Validators.required]),
-    age: new FormControl(null, [Validators.min(18), Validators.required]),
-    salary: new FormControl()});
+    age: new FormControl(null, [Validators.min(0)]),
+    salary: new FormControl(null, [Validators.min(0), Validators.required])});
 }
