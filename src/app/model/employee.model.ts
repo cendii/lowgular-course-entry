44d0607631e7923cc7acdefb55c9015c9d096efc
katarzynacendlak <kasia.cendlak@gmail.com>
2022-11-21 15:28:46 +0100

    feat: lesson 3, zad. 4.1

--- src/app/model/employee.model.ts
+++ src/app/model/employee.model.ts
@@ -1,5 +1,5 @@
 export interface EmployeeModel {
-  id: string;
+  personalNumber: string;
   name: string;
   email: string;
   image:string;
--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -2,6 +2,10 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { Observable } from 'rxjs';
 import {ProjectModel} from "../../model/project.model";
 import {ProjectService} from "../../services/project.service";
+import {EmployeeModel} from "../../model/employee.model";
+import {EmployeeService} from "../../services/employee.service";
+import {PersonModel} from "../../model/person.model";
+
 
 
 @Component({
@@ -11,8 +15,12 @@ import {ProjectService} from "../../services/project.service";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  data$: Observable<ProjectModel[] | null> = this._projectService.getAll();
+  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
 
-  constructor(private _projectService: ProjectService) {
+  constructor(private _employeeService: EmployeeService) {
   }
+  remove(personalNumber:string){
+    this._employeeService.delete(personalNumber).subscribe();
+  }
+
 }
