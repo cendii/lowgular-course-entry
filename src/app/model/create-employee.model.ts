34b9d876ddc91d08ef83822ef4b7b2154510220c
katarzynacendlak <kasia.cendlak@gmail.com>
2022-11-21 14:20:27 +0100

    feat: lesson 3, zad. 3.1 + 3.2

---
+++ src/app/model/create-employee.model.ts
@@ -0,0 +1,6 @@
+export interface CreateEmployeeModel {
+  readonly email: string;
+  readonly name: string;
+  readonly age: number;
+  readonly salary: string;
+}
--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -9,9 +11,19 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
-    name: new FormControl (null, [Validators.required]),
+    email: new FormControl( null, [Validators.required]),
     age: new FormControl(null, [Validators.min(0), Validators.required]),
-    salary: new FormControl(null, [Validators.min(0), Validators.required])});
+    salary: new FormControl(null, [Validators.min(0), Validators.required])
+  });
 
+  constructor(private _employeeService: EmployeeService) {
   }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create({ email: form.email, name: form.name, age: form.age, salary: form.salary}).subscribe();
+  }
+  onButtonClicked(form: { email: string, age: string, salary: string}) {
+    alert ('User was successfully added to the database. Email: ' + form.email +
+      ', Age: ' + form.age + ', Salary: ' + form.salary)}
+}
 
